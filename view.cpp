3da49aa95468a08aac39d36a03df631523c6d53a
racso-who <oscar-who@hotmail.com>
2014-07-15 04:47:14 -0400

    add labels and rage button, also input field for seed

--- view.cpp
+++ view.cpp
@@ -8,7 +8,7 @@
 
 // Creates buttons with labels. Sets butBox elements to have the same size, 
 // with 10 pixels between widgets
-View::View(Controller *c, Model *m) : model_(m), controller_(c), mainBox(true, 10), playerBox(true, 10), table("Cards on the table"), p1("Player 1"), p2("Player 2"), p3("Player 3"), p4("Player 4"), hand("Your hand"), panels(true,10), handBox(true, 10), butBox(true, 10), start_button( "Start new game with seed:" ), end_button( "End current game" ), rage_button("rage!"),  card(deck.null()) {
+View::View(Controller *c, Model *m) : model_(m), controller_(c), mainBox(true, 10), playerBox(true, 10), table("Cards on the table"), p1("Player 1"), p2("Player 2"), p3("Player 3"), p4("Player 4"), p1Box(true, 5), p2Box(true, 10), p3Box(true, 10), p4Box(true, 10), hand("Your hand"), panels(true,10), handBox(true, 10), butBox(true, 10), start_button( "Start new game with seed:" ), end_button( "End current game" ), p1Rage("Rage!"), p2Rage("Rage!"), p3Rage("Rage!"), p4Rage("Rage!"), p1Point("0 points"), p1Discard("0 discards"), p2Point("0 points"), p2Discard("0discards"), p3Point("0 points"), p3Discard("0 discards"), p4Point("0 points"), p4Discard("0 discards"), card(deck.null()) {
 
 	// Sets some properties of the window.
         set_title( "Straights" );
@@ -16,15 +16,42 @@ View::View(Controller *c, Model *m) : model_(m), controller_(c), mainBox(true, 1
 	add(mainBox);
 	mainBox.add(butBox);
 	butBox.add(start_button);
+
+	seedEntry.set_max_length(10);
+	seedEntry.select_region(0, seedEntry.get_text_length());
+	butBox.add(seedEntry);
 	butBox.add(end_button);
+
+	// middle table
 	mainBox.add(table);
 	table.add(card);
 	card.set(deck.null());
+	
+	// player section
 	mainBox.add(playerBox);
 	playerBox.add(p1);
+	p1.add(p1Box);
+	p1Box.add(p1Rage);
+	p1Box.add(p1Point);
+	p1Box.add(p1Discard);
 	playerBox.add(p2);
+	p2.add(p2Box);
+	p2Box.add(p2Rage);
+	p2Box.add(p2Point);
+	p2Box.add(p2Discard);
 	playerBox.add(p3);
+	p3.add(p3Box);
+	p3Box.add(p3Rage);
+	p3Box.add(p3Point);
+	p3Box.add(p3Discard);
+	hand.add(card);
 	playerBox.add(p4);
+	p4.add(p4Box);
+	p4Box.add(p4Rage);
+	p4Box.add(p4Point);
+	p4Box.add(p4Discard);
+
+	// area for your hand
 	mainBox.add(handBox);
 	handBox.add(hand);
 	
--- view.h
+++ view.h
@@ -28,13 +28,14 @@ private:
 	// Member widgets:
 	
 	Gtk::Frame table, p1, p2, p3, p4, hand;
-	Gtk::VBox panels, mainBox;      // Main window divided into two vertical panels
+	Gtk::VBox panels, mainBox, p1Box, p2Box, p3Box, p4Box;      // Main window divided into two vertical panels
 	Gtk::HBox butBox, playerBox, handBox;      // Vertical boxes for stacking buttons horizontally
 	Gtk::Button start_button;
 	Gtk::Button end_button;
-	Gtk::Button rage_button;
+	Gtk::Button p1Rage, p2Rage, p3Rage, p4Rage;
 	Gtk::Image card;
-
+	Gtk::Entry seedEntry;
+	Gtk::Label p1Point, p1Discard, p2Point, p2Discard, p3Point, p3Discard, p4Point, p4Discard;
 	// Signal handlers:
 	void startButtonClicked();
 	void endButtonClicked();
